94bbe65de1af947e489c5186b807a3c1cca19914
Jessica Marín <jmarin70190@ufide.ac.cr>
2025-02-20 20:48:47 -0600

    Semana 6 - php

---
+++ Semana6/php/holamundo.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hola mundo desde PHP</title>
+</head>
+
+<body>
+    <?php
+    echo "<p>Hola mundo en PHP</p>";
+    // declaracion de variables
+    $nombre = "Jess";//variable global, puede ser accedida desde cualquier funcion
+    $contador = 9;
+    $peso = 69.7;
+    $mayorEdad = true;
+
+    function miFunction()
+    {
+        $variableLocal = "Variable local"; //solo tiene vida dentre dela funcion
+        $nombre = "Juan";
+        echo $variableLocal;
+    }
+    echo $nombre;
+
+    miFunction();
+
+    $edad = 21;
+    //if simple
+    if ($edad < 18) {
+        echo "eres menor de edad";
+    } elseif ($edad == 18) {
+        echo "tienes 18";
+    } else {
+        echo "eres mayor de edad";
+        $mayorEdad = true;
+    }
+    //anidados
+    $membresia = true;
+    if ($edad >= 18) {
+        echo " Adulto";
+        if ($membresia) {
+            echo " miembro";
+        }
+    } else {
+        echo "menor de edad";
+        if ($membresia) {
+            echo " y miembro";
+        }
+    }
+    //switch
+    $nota = "B";
+    switch ($nota) {
+        case "A":
+            echo "Exc";
+            break;
+
+        case "B":
+            echo "Muy bien";
+            break;
+
+        case "C":
+            echo "Bien";
+            break;
+        default:
+            echo "Necesitas mejorar";
+            break;
+    }
+
+
+    ?>
+</body>
+
+</html>
