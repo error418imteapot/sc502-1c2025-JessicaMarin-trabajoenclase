b610bff2dcc54c7707aa4cb1452312539ad7872f
Jessica Marín <jmarin70190@ufide.ac.cr>
2025-04-12 11:06:06 -0600

    sem 12

---
+++ Semana12/backend/db.php
@@ -0,0 +1,15 @@
+<?php
+//nombres de las variables $:
+$host = "localhost";
+$dbname = "todo_app";
+$user= "root";
+$password = "Jm12345";
+try{
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // echo "Conexion exitosa ". PHP_EOL;
+    
+}catch(PDOException $e){
+    die("Error de conexion: " . $e -> getMessage());
+}
---
+++ Semana12/backend/tests/register_test.php
@@ -0,0 +1,5 @@
+<?php
+//los puntos son porque esta dos carpetas mas abajo
+require('../register.php');
+ 
+echo(userRegistry('user@gmail.com', '123', 'user@gmail.com'));
